1c6c7e9ad44b365291ae40d6513cf777737a2642
Aisha Tammy <floss@bsd.ac>
2020-12-12 19:38:16 -0500

    remove libevent from tests
    Signed-off-by: Aisha Tammy <floss@bsd.ac>

--- test/imsg_sendrcv.c
+++ test/imsg_sendrcv.c
@@ -19,12 +19,12 @@
 
 #include <err.h>
 #include <errno.h>
-#include <event.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/queue.h>
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
